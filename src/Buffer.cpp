0d8d1841c6031233eedfd0b4f4d77c1bb37a6446
Jeff Wright <jeffw387@gmail.com>
2018-10-29 19:39:56 -0700

    forgot return

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -8,28 +8,28 @@ Buffer::Buffer(
     VmaMemoryUsage memoryUsage,
     std::vector<uint32_t> queueIndices)
     : allocator(allocator) {
-      VkBufferCreateInfo bufferCreateInfo{};
-      bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-      bufferCreateInfo.usage = usage;
-      bufferCreateInfo.size = size;
+  VkBufferCreateInfo bufferCreateInfo{};
+  bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+  bufferCreateInfo.usage = usage;
+  bufferCreateInfo.size = size;
   bufferCreateInfo.queueFamilyIndexCount =
       static_cast<uint32_t>(queueIndices.size());
-      bufferCreateInfo.pQueueFamilyIndices = queueIndices.data();
+  bufferCreateInfo.pQueueFamilyIndices = queueIndices.data();
   bufferCreateInfo.sharingMode = queueIndices.size() > 1
                                      ? VK_SHARING_MODE_CONCURRENT
                                      : VK_SHARING_MODE_EXCLUSIVE;
-      
-      VmaAllocationCreateInfo allocationCreateInfo{};
-      allocationCreateInfo.usage = memoryUsage;
-      
+
+  VmaAllocationCreateInfo allocationCreateInfo{};
+  allocationCreateInfo.usage = memoryUsage;
+
   vmaCreateBuffer(
       allocator,
-        &bufferCreateInfo,
-        &allocationCreateInfo,
-        &buffer,
-        &allocation,
-        nullptr);
-    }
+      &bufferCreateInfo,
+      &allocationCreateInfo,
+      &buffer,
+      &allocation,
+      nullptr);
+}
 
 Buffer::~Buffer() {
   if (allocator != nullptr && buffer != VK_NULL_HANDLE &&
@@ -47,9 +47,7 @@ VmaAllocationInfo Buffer::getAllocationInfo() {
   return std::move(info);
 }
 
-uint32_t Buffer::memType() {
-  getAllocationInfo().memoryType;
-}
+uint32_t Buffer::memType() { return getAllocationInfo().memoryType; }
 
 VkDeviceMemory Buffer::deviceMemory() {
   return getAllocationInfo().deviceMemory;
