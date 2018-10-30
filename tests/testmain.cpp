4d89016b3bb89037941b30d1830d16f0e4eb6da3
Jeff Wright <jeffw387@gmail.com>
2018-10-30 10:22:32 -0700

    imgui pipeline create info

--- tests/testmain.cpp
+++ tests/testmain.cpp
@@ -541,6 +541,25 @@ struct AppState {
         VK_BLEND_OP_ADD,
         VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
             VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT);
+    imguiPipelineCreateInfo.addDynamicState(VK_DYNAMIC_STATE_VIEWPORT);
+    imguiPipelineCreateInfo.addDynamicState(VK_DYNAMIC_STATE_SCISSOR);
+    imguiPipelineCreateInfo.addShaderStage(VK_SHADER_STAGE_VERTEX_BIT,
+      {},
+      0,
+      nullptr,
+      *guiData.vertexShader,
+      "main");
+    imguiPipelineCreateInfo.addShaderStage(VK_SHADER_STAGE_FRAGMENT_BIT,
+      {},
+      0,
+      nullptr,
+      *guiData.fragmentShader,
+      "main");
+    imguiPipelineCreateInfo.addVertexAttribute(0, 0, VK_FORMAT_R32G32_SFLOAT, 0);
+    imguiPipelineCreateInfo.addVertexBinding(0, 8, VK_VERTEX_INPUT_RATE_VERTEX);
+    imguiPipelineCreateInfo.addViewportScissor({}, {});
+    imguiPipelineCreateInfo.setCullMode(VK_CULL_MODE_NONE);
+    //imguiPipelineCreateInfo.
     guiData.pipeline =
         device->createGraphicsPipeline(*pipelineCache, imguiPipelineCreateInfo);
 
