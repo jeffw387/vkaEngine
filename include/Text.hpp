ba6eb1366e2da3916a2ecc9bd4ceeb422f3fb5ab
Jeff Wright <jeffw387@gmail.com>
2018-11-02 08:48:27 -0700

    freetype wrapper work

--- include/Text.hpp
+++ include/Text.hpp
@@ -1,28 +1,49 @@
 #include "freetype2/ft2build.h"
 #include FT_FREETYPE_H
+#include FT_GLYPH_H
 #include <memory>
+#include <vector>
 #include <string>
 #include <optional>
+#include <gsl-lite.hpp>
 
 namespace FreeType {
 
-class GlyphSlot {
-public:
-
-private:
+struct BBox {
+  int xmin;
+  int ymin;
+  int xmax;
+  int ymax;
 };
 
-class Size {
+struct Dimensions {
+  uint32_t width;
+  uint32_t height;
+};
+
+class Glyph {
 public:
+  Glyph(FT_Glyph glyph);
+  ~Glyph();
+  gsl::span<unsigned char> render();
+  BBox getBoundingBox();
+  Dimensions getDimensions();
 private:
+  FT_Glyph glyph;
+  FT_BitmapGlyph bitmapGlyph;
 };
 
 class Face {
 public:
   Face(FT_Library library, std::string fontPath, FT_Long faceIndex);
-  GlyphSlot* loadGlyph(FT_ULong character);  
+  ~Face();
+  std::unique_ptr<Glyph> loadChar(FT_ULong character);
+  std::unique_ptr<Glyph> loadGlyph(FT_UInt glyphIndex);
+  std::vector<FT_ULong> getCharacters();
+  void setSize(uint8_t fontSize, FT_UInt dpi);
 private:
-  FT_Face* face;
+  FT_Library library;
+  FT_Face face;
 };
 
 class Font {
@@ -45,4 +66,13 @@ public:
 private:
   FT_Library library;
 };
+
+inline void testLoad() {
+  // std::string fontPath = "content/fonts/arial.ttf";
+  // Library library;
+  // auto font = library.loadFont(fontPath);
+  // auto face = font->createFace(0);
+  
+}
+
 }
