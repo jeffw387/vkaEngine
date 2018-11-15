c3144f043bb33b444993d426568698c1af2c05a1
Jeff Wright <jeffw387@gmail.com>
2018-11-14 17:02:54 -0800

    text stb implementation

--- include/Text.hpp
+++ include/Text.hpp
@@ -1,6 +1,3 @@
-// #include <ft2build.h>
-// #include FT_FREETYPE_H
-// #include FT_GLYPH_H
 #include <stb_truetype.h>
 #include <memory>
 #include <vector>
@@ -16,91 +13,61 @@
 
 namespace Text {
 
-struct Dimensions {
-  uint32_t width;
-  uint32_t height;
-};
+// using Tile = gsl::span<uint8_t>;
+// auto outputGlyphBitmap = [](const std::vector<uint8_t> &tile,
+//                             auto tileIndex,
+//                             Dimensions dimensions) {
+//   if (tile.size() == 0) return;
+//   auto fileName = std::string("glyph") + std::to_string(tileIndex) +
+//                   std::string("-") + std::to_string(dimensions.width) +
+//                   std::string("x") + std::to_string(dimensions.height) +
+//                   std::string(".buf");
+//   std::ofstream outFile{
+//       fileName,
+//       std::ios_base::out | std::ios_base::binary | std::ios_base::trunc};
+//   if (!outFile.is_open()) {
+//     MultiLogger::get()->error("File {} was not opened!", fileName);
+//     exit(1);
+//   }
+//   outFile.seekp(std::ios::beg);
+//   auto start = tile.data();
+//   auto size = tile.size();
 
-template <typename T>
-struct Rect {
-  T xmin;
-  T ymin;
-  T xmax;
-  T ymax;
-};
-
-using Tile = gsl::span<uint8_t>;
-auto outputGlyphBitmap = [](const std::vector<uint8_t> &tile,
-                            auto tileIndex,
-                            Dimensions dimensions) {
-  if (tile.size() == 0) return;
-  auto fileName = std::string("glyph") + std::to_string(tileIndex) +
-                  std::string("-") + std::to_string(dimensions.width) +
-                  std::string("x") + std::to_string(dimensions.height) +
-                  std::string(".buf");
-  std::ofstream outFile{
-      fileName,
-      std::ios_base::out | std::ios_base::binary | std::ios_base::trunc};
-  if (!outFile.is_open()) {
-    MultiLogger::get()->error("File {} was not opened!", fileName);
-    exit(1);
-  }
-  outFile.seekp(std::ios::beg);
-  auto start = tile.data();
-  auto size = tile.size();
-
-  outFile.write((char *)start, size);
-  if (outFile.fail()) {
-    MultiLogger::get()->error("Write operation to {} failed!", fileName);
-    if (outFile.bad()) {
-      MultiLogger::get()->error("Bad bit set!");
-    }
-    exit(1);
-  }
-};
-
-struct Tileset {
-  Tileset(
-      ranges::any_view<Tile> tiles,
-      uint32_t maxTilesetWidth,
-      uint32_t tileWidth,
-      uint32_t tileHeight);
-  ranges::any_view<Tile> tiles;
-  std::vector<Rect<float>> tileUVs;
-  std::vector<Rect<int32_t>> tileRects;
-  float width = {};
-  float height = {};
-};
-
-struct BitmapGlyph {
-  int32_t xmin;
-  int32_t ymin;
-  int32_t xmax;
-  int32_t ymax;
-  uint32_t width;
-  uint32_t height;
-  int32_t advance;
-  std::vector<uint8_t> bitmap;
-};
+//   outFile.write((char *)start, size);
+//   if (outFile.fail()) {
+//     MultiLogger::get()->error("Write operation to {} failed!", fileName);
+//     if (outFile.bad()) {
+//       MultiLogger::get()->error("Bad bit set!");
+//     }
+//     exit(1);
+//   }
+// };
 
 struct BasicCharacters {
   std::vector<uint8_t> characters;
-  constexpr BasicCharacters() {
-    ranges::view::closed_ints(32, 126) | ranges::action::push_back(characters);
+  BasicCharacters() {
+    ranges::action::push_back(
+        characters, ranges::view::closed_indices(32, 126));
   }
+};
+
+struct Atlas {
+  std::vector<uint8_t> pixels;
+  int width = {};
+  int height = {};
+  std::map<uint8_t, stbtt_packedchar> atlasData;
 };
 
 class Font {
 public:
   Font(std::string fontPath);
-  std::unique_ptr<BitmapGlyph> loadChar(int character);
-  std::unique_ptr<BitmapGlyph> loadGlyph(int glyphIndex);
-  std::map<int, std::unique_ptr<BitmapGlyph>> getGlyphs();
   void setPixelSize(uint32_t pixelSize);
+  Atlas getTextureAtlas(int width, int height);
 
 private:
   std::vector<uint8_t> fontBytes;
   stbtt_fontinfo fontInfo;
+  BasicCharacters charSet;
   float scale = 1.f;
 };
 }  // namespace Text
--- src/Text.cpp
+++ src/Text.cpp
@@ -4,88 +4,9 @@
 namespace Text {
 using namespace ranges;
 
-Tileset::Tileset(
-    any_view<Tile> tiles,
-    uint32_t maxTilesetWidth,
-    uint32_t tileWidth,
-    uint32_t tileHeight)
-    : tiles(tiles) {
-  int maxTilesPerRow = maxTilesetWidth / tileWidth;
-
-  auto tileRows = tiles | view::chunk(maxTilesPerRow);
-  auto tileRowCount = distance(tileRows);
-  width = maxTilesetWidth;
-  height = tileRowCount * tileHeight;
-
-  float rowIndex{};
-  RANGES_FOR(auto row, tileRows) {
-    float columnIndex{};
-    RANGES_FOR(auto tile, row) {
-      tileRects.push_back({static_cast<int32_t>(columnIndex * tileWidth),
-                           static_cast<int32_t>(rowIndex * tileHeight),
-                           static_cast<int32_t>((columnIndex + 1) * tileWidth),
-                           static_cast<int32_t>((rowIndex + 1) * tileHeight)});
-      tileUVs.push_back({(columnIndex * tileWidth) / width,
-                         (rowIndex * tileHeight) / height,
-                         ((columnIndex + 1) * tileWidth) / width,
-                         ((rowIndex + 1) * tileHeight) / height});
-      ++columnIndex;
-    }
-    ++rowIndex;
-  }
-}
-
-std::unique_ptr<BitmapGlyph> Font::loadChar(FT_ULong character) {
-  if (FT_Load_Char(face, character, FT_LOAD_RENDER)) {
-    MultiLogger::get()->error(
-        "Error loading character {} from face {}.",
-        character,
-        face->family_name);
-  }
-  return std::make_unique<BitmapGlyph>(face->glyph);
-}
-
-std::unique_ptr<BitmapGlyph> Font::loadGlyph(FT_UInt glyphIndex) {
-  if (FT_Load_Glyph(face, glyphIndex, FT_LOAD_RENDER)) {
-    MultiLogger::get()->error(
-        "Error loading glyph index {} from face {}.",
-        glyphIndex,
-        face->family_name);
-  }
-  return std::make_unique<BitmapGlyph>(face->glyph);
-}
-
-std::vector<FT_ULong> Font::getCharacters() {
-  FT_UInt glyphIndex{};
-  std::vector<FT_ULong> result;
-  result.push_back(FT_Get_First_Char(face, &glyphIndex));
-
-  while (true) {
-    result.push_back(FT_Get_Next_Char(face, result.back(), &glyphIndex));
-    if (!glyphIndex) {
-      break;
-    }
-  }
-  return result;
-}
-
-std::map<FT_ULong, std::unique_ptr<BitmapGlyph>> Font::getGlyphs() {
-  std::map<FT_ULong, std::unique_ptr<BitmapGlyph>> result;
-  for (auto character : getCharacters()) {
-    result[character] = loadChar(character);
-  }
-
-  MultiLogger::get()->info("Retrieved glyphs from face.");
-  return result;
-}
-
 void Font::setPixelSize(uint32_t pixelSize) {
-  if (FT_Set_Pixel_Sizes(face, 0, pixelSize)) {
-    MultiLogger::get()->error(
-        "Error setting font pixel height {} for face {}.",
-        pixelSize,
-        face->family_name);
-  }
+  scale = stbtt_ScaleForPixelHeight(&fontInfo, static_cast<float>(pixelSize));
+
   MultiLogger::get()->info("Font pixel height set to {}.", pixelSize);
 }
 
@@ -97,23 +18,29 @@ Font::Font(std::string fontPath) {
   }
 }
 
-std::unique_ptr<Face> Font::createFace(FT_Long faceIndex) {
-  return std::make_unique<Face>(library, fontBytes, faceIndex);
-}
+Atlas Font::getTextureAtlas(int width, int height) {
+  Atlas atlas{};
+  atlas.pixels.resize(width * height);
+  atlas.width = width;
+  atlas.height = height;
 
-Library::Library() {
-  if (FT_Init_FreeType(&library)) {
-    MultiLogger::get()->error("Error initializing freetype.");
+  stbtt_pack_context packContext{};
+  stbtt_PackBegin(
+      &packContext, atlas.pixels.data(), width, height, 0, 1, nullptr);
+  std::vector<stbtt_packedchar> atlasData;
+  atlasData.resize(charSet.characters.size());
+  stbtt_PackFontRange(
+      &packContext,
+      fontBytes.data(),
+      0,
+      scale,
+      32,
+      static_cast<int>(charSet.characters.size()),
+      atlasData.data());
+  stbtt_PackEnd(&packContext);
+  RANGES_FOR(auto pair, ranges::view::zip(charSet.characters, atlasData)) {
+    atlas.atlasData[std::get<0>(pair)] = std::get<1>(pair);
   }
-  MultiLogger::get()->info("Initialized freetype.");
 }
 
-Library::~Library() {
-  MultiLogger::get()->info("Destroying freetype library.");
-  FT_Done_FreeType(library);
-}
-
-std::unique_ptr<Font> Library::loadFont(std::string fontPath) {
-  return std::make_unique<Font>(library, fontPath);
-}
 }  // namespace Text
