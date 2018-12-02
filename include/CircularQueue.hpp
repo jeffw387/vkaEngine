4f64767ebf779594e3489bee147682f78fbc7db5
Jeff Wright <jeffw387@gmail.com>
2018-12-01 20:14:59 -0800

    removed ref wrapper stuff

--- include/CircularQueue.hpp
+++ include/CircularQueue.hpp
@@ -14,20 +14,20 @@ class CircularQueue
     mutable std::mutex storageMutex;
 
 public:
-    // returns a const reference to the first T if it exists
-    std::optional<std::reference_wrapper<const T>> readFirst() const
+    // returns the first T if it exists
+    std::optional<T> readFirst() const
     {
-        auto result = std::optional<std::reference_wrapper<const T>>();
+        auto result = std::optional<T>();
         if (count == 0)
         {
             return result;
         }
         std::lock_guard<std::mutex> lock(storageMutex);
-        result = std::cref(storage[firstID]);
+        result = storage[firstID];
         return result;
     }
 
-    // removes and returns the first T by value if it exists
+    // removes and returns the first T if it exists
     std::optional<T> popFirst()
     {
         auto first = readFirst();
@@ -41,21 +41,19 @@ public:
         firstID = newFirst;
         return result;
     }
+    
     template <typename PredicateType>
-    std::optional<T> popFirstIf(PredicateType p)
-    {
+    std::optional<T> popFirstIf(PredicateType p) {
         auto first = readFirst();
         std::optional<T> result;
         std::lock_guard<std::mutex> lock(storageMutex);
-        if (!first.has_value())
-            return result;
-        auto firstValue = first.value();
-        if (p(firstValue))
-        {
-            result = first.value();
-            auto newFirst = (++firstID) % S;
-            count--;
-            firstID = newFirst;
+        if (auto first = readFirst()) {
+          if (p(first.value())) {
+              result = first.value();
+              auto newFirst = (++firstID) % S;
+              count--;
+              firstID = newFirst;
+          }
         }
         return result;
     }
