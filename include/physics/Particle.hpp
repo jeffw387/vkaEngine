a429776bc785660d0d9ff74ace2fde5200f073f2
Jeff Wright <jeffw387@gmail.com>
2018-12-04 11:57:21 -0800

    collision resolution

--- include/physics/Particle.hpp
+++ include/physics/Particle.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <array>
 #include <glm/glm.hpp>
 
 namespace Physics {
@@ -7,13 +8,30 @@ class Particle {
 public:
   glm::vec3 position = {};
   glm::vec3 velocity = {};
-  glm::vec3 acceleration = {};
+  glm::vec3 constantAcceleration = {};
   glm::vec3 forceAccum = {};
   float damping = {};
   float inverseMass = {};
 
   void setMass(float mass);
   bool hasInfiniteMass() const;
+  void integrate(float timeScale);
+};
+
+struct ParticleContact {
+  std::array<Particle*, 2> particle = {};
+  float restitution = {};
+  glm::vec3 contactNormal = {};
+  float penetration = {};
+
+protected:
+  void resolve(float duration);
+  float calculateSeparatingVelocity() const;
+
+private:
+  float getTotalInverseMass() const;
+  void resolveVelocity(float duration, float totalInverseMass);
+  void resolveInterpenetration(float duration, float totalInverseMass);
 };
 
 class ParticleForceGenerator {
@@ -43,6 +61,15 @@ public:
   void updateForce(Particle* particle, float duration);
 };
 
+class ParticleAnchoredSpring : public ParticleForceGenerator {
+public:
+  glm::vec3 anchor;
+  float springConstant;
+  float restLength;
+
+  void updateForce(Particle* particle, float duration);
+};
+
 class ParticleForceRegistry {
 protected:
   struct ParticleForceRegistration {
@@ -62,8 +89,8 @@ public:
 
 inline Particle integrate(Particle input, float timeScale) {
   input.position += input.velocity * timeScale;
-  input.acceleration += input.forceAccum * input.inverseMass;
-  input.velocity += input.acceleration * timeScale;
+  input.constantAcceleration += input.forceAccum * input.inverseMass;
+  input.velocity += input.constantAcceleration * timeScale;
   input.velocity *= input.damping;
   input.forceAccum = {};
   return input;
--- src/Particle.cpp
+++ src/Particle.cpp
@@ -7,6 +7,71 @@ void Particle::setMass(float mass) { inverseMass = 1.f / mass; }
 
 bool Particle::hasInfiniteMass() const { return inverseMass == 0; }
 
+void Particle::integrate(float timeScale) {
+  position += velocity * timeScale;
+  constantAcceleration += forceAccum * inverseMass;
+  velocity += constantAcceleration * timeScale;
+  velocity *= damping;
+  forceAccum = {};
+}
+
+void ParticleContact::resolve(float duration) {
+  float totalInverseMass = getTotalInverseMass();
+  if (totalInverseMass > 0) {
+    resolveVelocity(duration, totalInverseMass);
+    if (penetration > 0) {
+      resolveInterpenetration(duration, totalInverseMass);
+    }
+  }
+}
+
+float ParticleContact::calculateSeparatingVelocity() const {
+  glm::vec3 relativeVelocity = particle[0]->velocity;
+  if (particle[1]) {
+    relativeVelocity -= particle[1]->velocity;
+  }
+  return glm::dot(relativeVelocity, contactNormal);
+}
+
+float ParticleContact::getTotalInverseMass() const {
+  float totalInverseMass = particle[0]->inverseMass;
+  if (particle[1]) {
+    totalInverseMass += particle[1]->inverseMass;
+  }
+  return totalInverseMass;
+}
+
+void ParticleContact::resolveVelocity(float duration, float totalInverseMass) {
+  float separatingVelocity = calculateSeparatingVelocity();
+  if (separatingVelocity > 0) {
+    return;
+  }
+
+  float newSeparatingVelocity = -separatingVelocity * restitution;
+  float deltaVelocity = newSeparatingVelocity - separatingVelocity;
+
+  float impulse = deltaVelocity / totalInverseMass;
+  glm::vec3 impulsePerInverseMass = contactNormal * impulse;
+
+  particle[0]->velocity += impulsePerInverseMass * particle[0]->inverseMass;
+
+  if (particle[1]) {
+    particle[1]->velocity += impulsePerInverseMass * particle[1]->inverseMass;
+  }
+}
+
+void ParticleContact::resolveInterpenetration(
+    float duration,
+    float totalInverseMass) {
+  glm::vec3 movePerInverseMass =
+      contactNormal * (-penetration / totalInverseMass);
+
+  particle[0]->position += movePerInverseMass * particle[0]->inverseMass;
+  if (particle[1]) {
+    particle[1]->position += movePerInverseMass * particle[1]->inverseMass;
+  }
+}
+
 void ParticleGravityGenerator::updateForce(Particle* particle, float duration) {
   if (particle->hasInfiniteMass()) {
     return;
@@ -27,9 +92,24 @@ void ParticleDragGenerator::updateForce(Particle* particle, float duration) {
 void ParticleSpring::updateForce(Particle* particle, float duration) {
   glm::vec3 force = particle->velocity;
   force -= other->position;
+
   float magnitude = force.length();
   magnitude = std::abs(magnitude - restLength);
   magnitude *= springConstant;
+
+  force = glm::normalize(force);
+  force *= -magnitude;
+  particle->forceAccum += force;
+}
+
+void ParticleAnchoredSpring::updateForce(Particle* particle, float duration) {
+  glm::vec3 force = particle->velocity;
+  force -= anchor;
+
+  float magnitude = force.length();
+  magnitude = std::abs(magnitude - restLength);
+  magnitude *= springConstant;
+
   force = glm::normalize(force);
   force *= -magnitude;
   particle->forceAccum += force;
