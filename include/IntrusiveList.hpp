ab2919b9b356d2acf01bdbea79b932153a753b87
Jeff Wright <jeffw387@gmail.com>
2018-12-04 19:22:08 -0800

    my quadtree and intrusive list implementations

---
+++ include/IntrusiveList.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+template <typename T>
+class IntrusiveList
+{
+public:
+    void push_front(T* element)
+    {
+        if (root != nullptr)
+        {
+            auto prevRoot = root;
+            prevRoot->previous = element;
+            element->next = prevRoot;
+        }
+        root = element;
+        ++count;
+    }
+
+    void pop_front()
+    {
+        root = root->next;
+        if (count > 0)
+            --count;
+    }
+
+    T* front()
+    {
+        return root;
+    }
+
+    void erase(T* element)
+    {
+        if (element->previous != nullptr)
+            element->previous->next = element->next;
+        if (element->next != nullptr)
+            element->next->previous = element->previous;
+        if (root == element)
+            root = nullptr;
+        if (count > 0)
+            --count;
+    }
+
+    const size_t& size() { return count; }
+private:
+    T* root = nullptr;
+    size_t count = 0U;
+};
