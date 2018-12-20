5c6d03482e12240c52d00ee3b0253455538b24c3
Jeff Wright <jeffw387@gmail.com>
2018-12-19 19:36:50 -0800

    trying to design api around futures

--- src/States.test.cpp
+++ src/States.test.cpp
@@ -1,6 +1,8 @@
 #include "States.hpp"
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include <future>
+#include <thread>
 
 TEST_CASE("Add state to history") {
   States<int, 3> states;
@@ -10,4 +12,8 @@ TEST_CASE("Add state to history") {
 
   REQUIRE(state);
   REQUIRE(**state == 0);
+}
+
+TEST_CASE("State points to data, future indicates when data is available") {
+  
 }
