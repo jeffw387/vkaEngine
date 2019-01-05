d4aee402c9626cc2a3dcea2d274e91a4262f87e2
Jeff Wright <jeffw387@gmail.com>
2019-01-05 08:08:20 -0800

    std-style naming

--- src/input.hpp
+++ src/input.hpp
@@ -8,69 +8,69 @@
 
 namespace input {
 
-struct Action {
+struct action {
   std::string name;
 };
 
-inline bool operator==(Action a, Action b) { return a.name == b.name; }
+inline bool operator==(action a, action b) { return a.name == b.name; }
 
-struct Signature {
+struct signature {
   int code;
   int action;
 };
 
 template <typename T>
-struct Event {
-  Signature signature;
+struct event {
+  signature signature;
   vka::Clock::time_point eventTime;
 };
 
-inline bool operator==(Signature a, Signature b) {
+inline bool operator==(signature a, signature b) {
   return a.code == b.code && a.action == b.action;
 }
 
-struct Key;
-struct Mouse;
-using InputEvent = std::variant<Event<Key>, Event<Mouse>>;
-class Manager {
+struct key;
+struct mouse;
+using input_event = std::variant<event<key>, event<mouse>>;
+class manager {
 public:
-  void addInputToQueue(InputEvent inputEvent) {
-    inputQueue.push_last(inputEvent);
+  void enqueue(input_event inputEvent) {
+    m_inputQueue.push_last(inputEvent);
   };
-  auto getEventBefore(vka::Clock::time_point cutoff) {
-    if (auto eventOptional = inputQueue.first_if([=](auto inputEvent) {
+  auto next_event_before(vka::Clock::time_point cutoff) {
+    if (auto eventOptional = m_inputQueue.first_if([=](auto inputEvent) {
           return std::visit(
               [=](const auto& inputVariant) {
                 return inputVariant.eventTime < cutoff;
               },
               inputEvent);
         })) {
-      inputQueue.pop_first();
+      m_inputQueue.pop_first();
       return eventOptional;
     }
   }
 
 private:
-  FlatList<InputEvent, 256> inputQueue;
+  FlatList<input_event, 256> m_inputQueue;
 };
 
-using Bindings = std::unordered_multimap<Action, Signature>;
-using InverseBindings = std::unordered_map<Signature, Action>;
-using State = std::unordered_map<Action, bool>;
+using bindings = std::unordered_multimap<action, signature>;
+using inverse_bindings = std::unordered_map<signature, action>;
+using state = std::unordered_map<action, bool>;
 }  // namespace input
 
 namespace std {
 template <>
-struct hash<input::Signature> {
-  size_t operator()(input::Signature value) const {
-    return SpookyHash::Hash64(&value, sizeof(input::Signature), 0);
+struct hash<input::signature> {
+  size_t operator()(input::signature value) const {
+    return SpookyHash::Hash64(&value, sizeof(input::signature), 0);
   }
 };
 
 template <>
-struct hash<input::Action> {
-  size_t operator()(input::Action value) const {
-    return SpookyHash::Hash64(&value, sizeof(input::Action), 0);
+struct hash<input::action> {
+  size_t operator()(input::action value) const {
+    return SpookyHash::Hash64(&value, sizeof(input::action), 0);
   }
 };
 }  // namespace std
