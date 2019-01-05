ecdab9b9305ea5bbe68b73dd540459c03c9a1af5
Jeff Wright <jeffw387@gmail.com>
2019-01-05 07:59:19 -0800

    outdated test in wrong file

--- src/instance.test.cpp
+++ src/instance.test.cpp
@@ -13,18 +13,3 @@ TEST_CASE("Create an instance") {
   instance_ptr = std::move(instance_result.value());
   REQUIRE(instance_ptr->operator VkInstance() != VK_NULL_HANDLE);
 }
-
-TEST_CASE("Create surface") {
-  auto window = platform::GLFW::createWindow(100, 100, "window title");
-  auto glfwExtensions = platform::GLFW::getRequiredInstanceExtensions();
-  auto builder = vka::instance_builder{};
-  builder.set_api_version(1, 0, 0);
-  for (auto extension : glfwExtensions) {
-    builder.add_extension(extension);
-  }
-  auto instance_result = builder.build();
-  auto& instance = instance_result.value();
-  auto surface = platform::GLFW::createSurface(*instance, window.value());
-  REQUIRE(surface);
-  REQUIRE(surface.value() != VK_NULL_HANDLE);
-}
