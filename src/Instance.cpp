0668e80e7ce497b110ca2cd8bf7054051f7c1a23
Jeff Wright <jeffw387@gmail.com>
2019-01-04 17:11:15 -0800

    duplicate files on windows, only keep lower-case versions

--- src/Instance.cpp
+++
@@ -1,9 +0,0 @@
-#include "Instance.hpp"
-#include <GLFW/glfw3.h>
-#include "Device.hpp"
-#include "Surface.hpp"
-#include <stdexcept>
-#include <iostream>
-#include "spdlog/spdlog.h"
-
-namespace vka {}  // namespace vka
