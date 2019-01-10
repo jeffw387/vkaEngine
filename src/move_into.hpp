2549e52b222e315c67f63e68d82062599e7135f7
Jeff Wright <jeffw387@gmail.com>
2019-01-10 15:30:50 -0800

    helper functors

--- src/move_into.hpp
+++ src/move_into.hpp
@@ -2,10 +2,16 @@
 
 template <typename T>
 struct move_into{
-  move_into(T& valueReference) : m_valueReference(valueReference) {};
-  void operator()(T value) {
-    m_valueReference = std::move(value);
+  move_into(T& valueReference) : m_destinationReference(valueReference) {};
+  void operator()(T destination) {
+    m_destinationReference = std::move(destination);
   }
 private:
-  T& m_valueReference;
+  T& m_destinationReference;
+};
+
+template <typename T>
+struct move_value_into {
+  move_value_into(T value, T& destination) { destination = std::move(value); }
+  void operator()() {}
 };
