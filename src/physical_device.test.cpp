0780118220bd0876a68497d2589680e32369335f
Jeff Wright <jeffw387@gmail.com>
2019-01-10 12:01:34 -0800

    monadic pattern for errors

--- src/physical_device.test.cpp
+++ src/physical_device.test.cpp
@@ -1,15 +1,24 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include <memory>
 #include "physical_device.hpp"
 #include "instance.hpp"
 
 using namespace vka;
 TEST_CASE("Select first physical device") {
-  auto instanceResult = instance_builder{}.build();
-  REQUIRE(instanceResult);
-  auto physicalDeviceResult =
-      physical_device_selector{}.select(**instanceResult);
-  REQUIRE(physicalDeviceResult);
-  REQUIRE(physicalDeviceResult.value());
-  REQUIRE(**physicalDeviceResult != VK_NULL_HANDLE);
+  std::unique_ptr<instance> instancePtr = {};
+  instance_builder{}
+      .build()
+      .map([& d = instancePtr](auto value) { d = std::move(value); })
+      .map_error([](auto error) { REQUIRE(false); });
+  VkPhysicalDevice physicalDevice = {};
+  physical_device_selector{}
+      .select(*instancePtr)
+      .map([& d = physicalDevice](auto value) {
+        value.map([& d = d](auto value) { d = value; }).or_else([] {
+          REQUIRE(false);
+        });
+      })
+      .map_error([](auto error) { REQUIRE(false); });
+  REQUIRE(physicalDevice != VK_NULL_HANDLE);
 }
