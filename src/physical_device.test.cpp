e4030f2d2e13307d8180a72a2b0125209f2c979e
Jeff Wright <jeffw387@gmail.com>
2019-01-10 12:20:33 -0800

    simplifying creation code with move_into, unified error type

--- src/physical_device.test.cpp
+++ src/physical_device.test.cpp
@@ -3,22 +3,19 @@
 #include <memory>
 #include "physical_device.hpp"
 #include "instance.hpp"
+#include "move_into.hpp"
 
 using namespace vka;
 TEST_CASE("Select first physical device") {
   std::unique_ptr<instance> instancePtr = {};
   instance_builder{}
       .build()
-      .map([& d = instancePtr](auto value) { d = std::move(value); })
+      .map(move_into{instancePtr})
       .map_error([](auto error) { REQUIRE(false); });
   VkPhysicalDevice physicalDevice = {};
   physical_device_selector{}
       .select(*instancePtr)
-      .map([& d = physicalDevice](auto value) {
-        value.map([& d = d](auto value) { d = value; }).or_else([] {
-          REQUIRE(false);
-        });
-      })
+      .map(move_into{physicalDevice})
       .map_error([](auto error) { REQUIRE(false); });
   REQUIRE(physicalDevice != VK_NULL_HANDLE);
 }
