94f56b97a07f83155f55ec09640f06cced0984bc
Jeff Wright <jeffw387@gmail.com>
2019-01-10 15:30:11 -0800

    monadic error handling

--- src/queue_family.test.cpp
+++ src/queue_family.test.cpp
@@ -5,6 +5,7 @@
 #include "instance.hpp"
 #include "physical_device.hpp"
 #include "platform_glfw.hpp"
+#include "move_into.hpp"
 
 using namespace vka;
 TEST_CASE("Select a graphics/present queue") {
@@ -13,23 +14,31 @@ TEST_CASE("Select a graphics/present queue") {
   for (auto extension : surfaceExtensions) {
     instanceBuilder.add_extension(extension);
   }
-  auto instanceResult =
-      instanceBuilder.add_layer("VK_LAYER_LUNARG_standard_validation").build();
-  REQUIRE(instanceResult);
-  auto physicalDeviceResult =
-      physical_device_selector{}.select(**instanceResult);
-  REQUIRE(physicalDeviceResult);
-  REQUIRE(physicalDeviceResult.value());
-  auto surfaceResult = surface_builder{}
+  std::unique_ptr<instance> instancePtr = {};
+  instanceBuilder.add_layer("VK_LAYER_LUNARG_standard_validation")
+      .build()
+      .map(move_into{instancePtr})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  VkPhysicalDevice physicalDevice = {};
+  physical_device_selector{}
+      .select(*instancePtr)
+      .map(move_into{physicalDevice})
+      .map_error([](auto error) { REQUIRE(false); });
+  std::unique_ptr<surface> surfacePtr = {};
+  surface_builder{}
                            .width(100)
                            .height(100)
                            .title("test window")
-                           .build(**instanceResult);
-  REQUIRE(surfaceResult);
-  auto queueFamilyResult = queue_family_builder{}
+      .build(*instancePtr)
+      .map(move_into{surfacePtr})
+      .map_error([](auto error) { REQUIRE(false); });
+  queue_family queueFamily = {};
+  queue_family_builder{}
                                .graphics_support()
-                               .present_support(**surfaceResult)
+      .present_support(*surfacePtr)
                                .queue(1.f)
-                               .build(**physicalDeviceResult);
-  REQUIRE(queueFamilyResult);
+      .build(physicalDevice)
+      .map(move_into{queueFamily})
+      .map_error([](auto error) { REQUIRE(false); });
 }
