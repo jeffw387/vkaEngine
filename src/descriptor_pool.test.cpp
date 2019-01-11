5594a9c18e19eadb8d5575659a014c70d809da6e
Jeff Wright <jeffw387@gmail.com>
2019-01-10 16:12:49 -0800

    monadic error handling

--- src/descriptor_pool.test.cpp
+++ src/descriptor_pool.test.cpp
@@ -6,42 +6,47 @@
 #include "physical_device.hpp"
 #include "queue_family.hpp"
 #include "device.hpp"
+#include "move_into.hpp"
 
 using namespace platform;
 using namespace vka;
 TEST_CASE("Create descriptor pool") {
-  glfw::init();
-  std::unique_ptr<instance> instancePtr = {};
-  auto instanceBuilder = instance_builder{}.add_layer(standard_validation);
-  auto surfaceExtensions = platform::glfw::get_required_instance_extensions();
-  for (auto extension : surfaceExtensions) {
-    instanceBuilder.add_extension(extension);
-  }
-  instanceBuilder.build()
-    .map([&d = instancePtr](auto value){ d = std::move(value); })
-    .map_error([](auto error){ REQUIRE(false); });
-  VkPhysicalDevice physicalDevice = {};
-  physical_device_selector{}.select(*instancePtr)
-    .map([&d = physicalDevice](auto value) { REQUIRE_NOTHROW(d = std::move(value.value())); })
-    .map_error([](auto error) { REQUIRE(false); });
+  platform::glfw::init();
+  std::unique_ptr<vka::instance> instancePtr = {};
+  vka::instance_builder{}
+      .add_layer(vka::standard_validation)
+      .build()
+      .map(move_into{instancePtr})
+      .map_error([](auto error) { REQUIRE(false); });
 
-  queue_family graphicsPresentFamily = {};
-      queue_family_builder{}.graphics_support().queue(1.f).build(
-          physicalDevice);
-  REQUIRE(queueFamilyResult);
-  auto deviceResult = device_builder{}
-                          .add_queue_family(*queueFamilyResult)
-                          .physical_device(physicalDevice)
-                          .extension(swapchain_extension)
-                          .build(*instancePtr);
-  REQUIRE(deviceResult);
-  auto descriptorPoolResult =
-      descriptor_pool_builder{}
-          .add_type({VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1})
-          .max_sets(1)
-          .build(**deviceResult);
-  REQUIRE(descriptorPoolResult);
-  REQUIRE(*descriptorPoolResult);
-  REQUIRE(
-      (**descriptorPoolResult).operator VkDescriptorPool() != VK_NULL_HANDLE);
+  VkPhysicalDevice physicalDevice = {};
+  physical_device_selector{}
+      .select(*instancePtr)
+      .map(move_into{physicalDevice})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  queue_family queueFamily = {};
+  queue_family_builder{}
+      .graphics_support()
+      .queue(1.f)
+      .build(physicalDevice)
+      .map(move_into{queueFamily})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  std::unique_ptr<device> devicePtr = {};
+  device_builder{}
+      .add_queue_family(queueFamily)
+      .physical_device(physicalDevice)
+      .build(*instancePtr)
+      .map(move_into{devicePtr})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  std::unique_ptr<descriptor_pool> descriptorPoolPtr = {};
+  descriptor_pool_builder{}
+      .add_type({VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1})
+      .max_sets(1)
+      .build(*devicePtr)
+      .map(move_into{descriptorPoolPtr})
+      .map_error([](auto error) { REQUIRE(false); });
+  REQUIRE(descriptorPoolPtr->operator VkDescriptorPool() != VK_NULL_HANDLE);
 }
