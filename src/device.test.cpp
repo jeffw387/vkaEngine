a9c8397add188dce71ceb0f900bb899328418476
Jeff Wright <jeffw387@gmail.com>
2019-01-10 16:08:49 -0800

    monadic error handling

--- src/device.test.cpp
+++ src/device.test.cpp
@@ -4,24 +4,39 @@
 #include "instance.hpp"
 #include "physical_device.hpp"
 #include "queue_family.hpp"
+#include "platform_glfw.hpp"
+#include "move_into.hpp"
 
 using namespace vka;
 TEST_CASE("Create a device") {
-  auto instanceResult =
-      instance_builder{}.add_layer(standard_validation).build();
-  REQUIRE(instanceResult);
-  auto physicalDeviceResult =
-      physical_device_selector{}.select(**instanceResult);
-  REQUIRE(physicalDeviceResult);
-  auto graphicsQueueFamily =
-      queue_family_builder{}.graphics_support().queue(1.f).build(
-          **physicalDeviceResult);
-  REQUIRE(graphicsQueueFamily);
-  auto deviceResult = device_builder{}
-                          .physical_device(**physicalDeviceResult)
-                          .add_queue_family(*graphicsQueueFamily)
-                          .build(**instanceResult);
-  REQUIRE(deviceResult);
-  REQUIRE(*deviceResult);
-  REQUIRE((**deviceResult).operator VkDevice() != VK_NULL_HANDLE);
+  platform::glfw::init();
+  std::unique_ptr<vka::instance> instancePtr = {};
+  vka::instance_builder{}
+      .add_layer(vka::standard_validation)
+      .build()
+      .map(move_into{instancePtr})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  VkPhysicalDevice physicalDevice = {};
+  physical_device_selector{}
+      .select(*instancePtr)
+      .map(move_into{physicalDevice})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  queue_family queueFamily = {};
+  queue_family_builder{}
+      .graphics_support()
+      .queue(1.f)
+      .build(physicalDevice)
+      .map(move_into{queueFamily})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  std::unique_ptr<device> devicePtr = {};
+  device_builder{}
+      .add_queue_family(queueFamily)
+      .physical_device(physicalDevice)
+      .build(*instancePtr)
+      .map(move_into{devicePtr})
+      .map_error([](auto error) { REQUIRE(false); });
+  REQUIRE(devicePtr->operator VkDevice() != VK_NULL_HANDLE);
 }
