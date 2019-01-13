cc7c62328f4b03f525aadb2bc1109db81d3eabc9
Jeff Wright <jeffw387@gmail.com>
2019-01-12 21:20:40 -0800

    added command pool with tests

---
+++ src/command_pool.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <vulkan/vulkan.h>
+#include <tl/expected.hpp>
+#include <memory>
+
+namespace vka {
+struct command_pool {
+  explicit command_pool(
+      VkDevice device,
+      VkCommandPool pool,
+      bool canResetBuffers)
+      : m_device(device), m_pool(pool), m_canResetBuffers(canResetBuffers) {}
+  command_pool(const command_pool&) = delete;
+  command_pool(command_pool&&) = default;
+  command_pool& operator=(const command_pool&) = delete;
+  command_pool& operator=(command_pool&&) = default;
+  ~command_pool() { vkDestroyCommandPool(m_device, m_pool, nullptr); }
+  operator VkCommandPool() { return m_pool; }
+  bool can_reset_buffers() const noexcept { return m_canResetBuffers; }
+
+private:
+  VkDevice m_device = {};
+  VkCommandPool m_pool = {};
+  bool m_canResetBuffers = {};
+};
+
+struct command_pool_builder {
+  tl::expected<std::unique_ptr<command_pool>, VkResult> build(VkDevice device) {
+    VkCommandPoolCreateInfo createInfo = {
+        VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO};
+    createInfo.queueFamilyIndex = m_queueFamilyIndex;
+    createInfo.flags =
+        m_canResetBuffers ? VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT : 0;
+
+    VkCommandPool pool = {};
+    auto result = vkCreateCommandPool(device, &createInfo, nullptr, &pool);
+    if (result != VK_SUCCESS) {
+      return tl::make_unexpected(result);
+    }
+
+    return std::make_unique<command_pool>(device, pool, m_canResetBuffers);
+  }
+
+  command_pool_builder& queue_family_index(uint32_t index) {
+    m_queueFamilyIndex = index;
+    return *this;
+  }
+
+  command_pool_builder& allow_buffer_reset() {
+    m_canResetBuffers = true;
+    return *this;
+  }
+
+private:
+  uint32_t m_queueFamilyIndex = {};
+  bool m_canResetBuffers = {};
+};
+}  // namespace vka
---
+++ src/command_pool.test.cpp
@@ -0,0 +1,50 @@
+#include "command_pool.hpp"
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "device.hpp"
+#include "instance.hpp"
+#include "physical_device.hpp"
+#include "queue_family.hpp"
+#include "platform_glfw.hpp"
+#include "move_into.hpp"
+
+using namespace vka;
+TEST_CASE("Create a command pool") {
+  platform::glfw::init();
+  std::unique_ptr<vka::instance> instancePtr = {};
+  vka::instance_builder{}
+      .add_layer(vka::standard_validation)
+      .build()
+      .map(move_into{instancePtr})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  VkPhysicalDevice physicalDevice = {};
+  physical_device_selector{}
+      .select(*instancePtr)
+      .map(move_into{physicalDevice})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  queue_family queueFamily = {};
+  queue_family_builder{}
+      .graphics_support()
+      .queue(1.f)
+      .build(physicalDevice)
+      .map(move_into{queueFamily})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  std::unique_ptr<device> devicePtr = {};
+  device_builder{}
+      .add_queue_family(queueFamily)
+      .physical_device(physicalDevice)
+      .build(*instancePtr)
+      .map(move_into{devicePtr})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  std::unique_ptr<command_pool> commandPoolPtr = {};
+  command_pool_builder{}
+      .queue_family_index(queueFamily.familyIndex)
+      .build(*devicePtr)
+      .map(move_into{commandPoolPtr})
+      .map_error([](auto error) { REQUIRE(false); });
+  REQUIRE(commandPoolPtr->operator VkCommandPool() != VK_NULL_HANDLE);
+}
