c6e69f0da27f30e65e0ec5ed47445dd5a985b4d8
Jeff Wright <jeffw387@gmail.com>
2019-01-12 17:42:10 -0800

    setup pool from layout test

--- src/descriptor_pool.test.cpp
+++ src/descriptor_pool.test.cpp
@@ -41,12 +41,34 @@ TEST_CASE("Create descriptor pool") {
       .map(move_into{devicePtr})
       .map_error([](auto error) { REQUIRE(false); });
 
-  std::unique_ptr<descriptor_pool> descriptorPoolPtr = {};
-  descriptor_pool_builder{}
-      .add_type({VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1})
-      .max_sets(1)
-      .build(*devicePtr)
-      .map(move_into{descriptorPoolPtr})
-      .map_error([](auto error) { REQUIRE(false); });
-  REQUIRE(descriptorPoolPtr->operator VkDescriptorPool() != VK_NULL_HANDLE);
+  SECTION("Add types individually") {
+    std::unique_ptr<descriptor_pool> descriptorPoolPtr = {};
+    descriptor_pool_builder{}
+        .add_type(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1)
+        .max_sets(1)
+        .build(*devicePtr)
+        .map(move_into{descriptorPoolPtr})
+        .map_error([](auto error) { REQUIRE(false); });
+    REQUIRE(descriptorPoolPtr->operator VkDescriptorPool() != VK_NULL_HANDLE);
+  }
+
+  SECTION("Add types from a layout") {
+    std::unique_ptr<descriptor_set_layout> layoutPtr = {};
+    descriptor_set_layout_builder{}
+        .image(0, 3, VK_SHADER_STAGE_FRAGMENT_BIT)
+        .sampler(1, 1, VK_SHADER_STAGE_FRAGMENT_BIT, {})
+        .uniform_buffer(2, 1, VK_SHADER_STAGE_VERTEX_BIT)
+        .uniform_buffer(3, 2, VK_SHADER_STAGE_FRAGMENT_BIT)
+        .build(*devicePtr)
+        .map(move_into{layoutPtr})
+        .map_error([](auto error) { REQUIRE(false); });
+
+    std::unique_ptr<descriptor_pool> descriptorPoolPtr = {};
+    descriptor_pool_builder{}
+        .add_layout(layoutPtr.get(), 3)
+        .build(*devicePtr)
+        .map(move_into{descriptorPoolPtr})
+        .map_error([](auto error) { REQUIRE(false); });
+    REQUIRE(descriptorPoolPtr->operator VkDescriptorPool() != VK_NULL_HANDLE);
+  }
 }
