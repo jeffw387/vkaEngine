e9f9cf23981bbdcbcfb248da5fe727be2d510d54
Jeff Wright <jeffw387@gmail.com>
2019-01-14 18:18:18 -0800

    add queue with test

---
+++ src/queue.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <vulkan/vulkan.h>
+#include <tl/expected.hpp>
+#include "queue_family.hpp"
+
+namespace vka {
+struct queue {
+  queue() = default;
+  explicit queue(VkQueue queueHandle)
+  : m_queue(queueHandle) {}
+  operator VkQueue() { return m_queue; }
+private:
+  VkQueue m_queue = {};
+};
+
+struct queue_index_out_of_bounds {};
+
+struct queue_builder {
+  tl::expected<queue, queue_index_out_of_bounds> build(VkDevice device) {
+    if (m_queueIndex >= m_queueFamily.queuePriorities.size()) {
+      return tl::make_unexpected(queue_index_out_of_bounds{});
+    }
+    VkQueue queueHandle = {};
+    vkGetDeviceQueue(device, m_queueFamily.familyIndex, m_queueIndex, &queueHandle);
+    return queue(queueHandle);
+  }
+
+  queue_builder& queue_info(queue_family family, uint32_t queueIndex) {
+    m_queueFamily = family;
+    m_queueIndex = queueIndex;
+    return *this;
+  }
+
+private:
+  queue_family m_queueFamily = {};
+  uint32_t m_queueIndex = {};
+};
+
+}
---
+++ src/queue.test.cpp
@@ -0,0 +1,50 @@
+#include "queue.hpp"
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "platform_glfw.hpp"
+#include "instance.hpp"
+#include "physical_device.hpp"
+#include "queue_family.hpp"
+#include "device.hpp"
+#include "move_into.hpp"
+
+using namespace vka;
+TEST_CASE("Get queue from device") {
+  platform::glfw::init();
+  std::unique_ptr<instance> instancePtr = {};
+  instance_builder{}
+      .add_layer(standard_validation)
+      .build()
+      .map(move_into{instancePtr})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  VkPhysicalDevice physicalDevice = {};
+  physical_device_selector{}
+      .select(*instancePtr)
+      .map(move_into{physicalDevice})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  queue_family queueFamily = {};
+  queue_family_builder{}
+      .graphics_support()
+      .queue(1.f)
+      .build(physicalDevice)
+      .map(move_into{queueFamily})
+      .map_error([](auto error) { REQUIRE(false); });
+
+  std::unique_ptr<device> devicePtr = {};
+  device_builder{}
+      .add_queue_family(queueFamily)
+      .physical_device(physicalDevice)
+      .build(*instancePtr)
+      .map(move_into{devicePtr})
+      .map_error([](auto error) { REQUIRE(false); });
+  
+  queue queueHandle;
+  queue_builder{}
+    .queue_info(queueFamily, 0)
+    .build(*devicePtr)
+    .map(move_into{queueHandle})
+    .map_error([](auto error) {REQUIRE(false); });
+  REQUIRE(queueHandle.operator VkQueue() != VK_NULL_HANDLE);
+}
