24b77aef887d4b5b251a9f39731bd0dd196f45ae
Jeff Wright <jeffw387@gmail.com>
2019-01-23 23:19:06 -0800

    updated vma to 2.2

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class vkaEngineConan(ConanFile):
     generators = "cmake"
     exports_sources = "!build/*", "!.vs", "!.vscode", "!.git", "*"
     build_policy = "missing"
-    requires = "vulkan-sdk/1.X.X@jeffw387/testing", "glfw/3.2.1@sesame/stable", "glm/0.9.9.1@g-truc/stable", "VulkanMemoryAllocator/2.0.0@sesame/stable", "spdlog/1.3.0@bincrafters/stable", "stb/20180214@conan/stable", "jsonformoderncpp/3.5.0@vthiery/stable", "Catch2/2.5.0@catchorg/stable", "tl_expected/0.2@jeffw387/testing", "tl_optional/0.5@jeffw387/testing"
+    requires = "vulkan-sdk/1.X.X@jeffw387/testing", "glfw/3.2.1@sesame/stable", "glm/0.9.9.1@g-truc/stable", "VulkanMemoryAllocator/2.2.0@jeffw387/testing", "spdlog/1.3.0@bincrafters/stable", "stb/20180214@conan/stable", "jsonformoderncpp/3.5.0@vthiery/stable", "Catch2/2.5.0@catchorg/stable", "tl_expected/0.2@jeffw387/testing", "tl_optional/0.5@jeffw387/testing"
         
     def build(self):
         cmake = CMake(self)
