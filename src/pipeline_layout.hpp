9583f9c13f1f461b78b12fb42d913fe55049c3c4
Jeff Wright <jeffw387@gmail.com>
2019-02-06 23:34:17 -0800

    make_pipeline_layout added

--- src/pipeline_layout.hpp
+++ src/pipeline_layout.hpp
@@ -4,6 +4,8 @@
 #include <tl/expected.hpp>
 #include <memory>
 #include <vector>
+#include <make_shader.hpp>
+#include "descriptor_set_layout.hpp"
 
 namespace vka {
 struct pipeline_layout {
@@ -23,5 +25,40 @@ private:
   VkPipelineLayout m_layout = {};
 };
 
+inline auto make_pipeline_layout(
+    VkDevice device,
+    std::vector<jshd::shader_data> shaderData,
+    std::vector<set_data> setData) {
+  std::vector<VkDescriptorSetLayout> layouts;
+  layouts.reserve(setData.size());
+  std::vector<VkPushConstantRange> pushRanges;
 
+  for (const set_data& set : setData) {
+    layouts.push_back(*set.setLayoutPtr);
+  }
+
+  for (const jshd::shader_data& shader : shaderData) {
+    for (const auto& push : shader.pushConstants) {
+      // TODO: need range from shader for each push constant
+      pushRanges.push_back(
+          {VkShaderStageFlags{} | shader.stage,
+           push.offset,
+           std::visit(
+               [](auto v) { return static_cast<uint32_t>(sizeof(v)); },
+               push.glslType)});
+    }
+  }
+
+  VkPipelineLayoutCreateInfo createInfo{VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO};
+  createInfo.setLayoutCount = static_cast<uint32_t>(layouts.size());
+  createInfo.pSetLayouts = layouts.data();
+  createInfo.pushConstantRangeCount = static_cast<uint32_t>(pushRanges.size());
+  createInfo.pPushConstantRanges = pushRanges.data();
+  VkPipelineLayout pipelineLayout{};
+  auto createResult = vkCreatePipelineLayout(device, &createInfo, nullptr, &pipelineLayout);
+  if (createResult != VK_SUCCESS) {
+    exit(createResult);
+  }
+  return std::make_unique<pipeline_layout>(device, pipelineLayout);
+}
 }  // namespace vka
