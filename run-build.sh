e01363135be25d22ee55282e202f1b62bc57bd2c
Jeff Wright <jeffw387@gmail.com>
2019-02-08 18:59:11 -0800

    forgot echo on

--- run-build.sh
+++ run-build.sh
@@ -3,5 +3,5 @@
 ./make-profiles.sh
 mkdir -p build
 cd build
-echo -e conan install .. --build=missing --profile=$CONANPROFILE
+conan install .. --build=missing --profile=$CONANPROFILE
 conan build ..
