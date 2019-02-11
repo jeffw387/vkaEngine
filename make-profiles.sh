04bfde892f9561d3b1bcef32d36093e2c4350810
Jeff Wright <jeffw387@gmail.com>
2019-02-11 13:32:32 -0800

    lower-case filenames, libcxx to 11

--- make-profiles.sh
+++ make-profiles.sh
@@ -7,7 +7,7 @@ GCC_PR=./profiles/gccprofile
 CLANG_PR=./profiles/clangprofile
 conan profile new --detect $GCC_PR
 conan profile new --detect $CLANG_PR
-conan profile update settings.compiler.libcxx=libstdc++ $GCC_PR;
+conan profile update settings.compiler.libcxx=libstdc++11 $GCC_PR;
 conan profile update settings.compiler.libcxx=libc++ $CLANG_PR;
 conan profile update settings.cppstd=17 $GCC_PR
 conan profile update settings.cppstd=17 $CLANG_PR
